6cf81146a978b65e7bcb75e663bdf6c21a60c211
Khush Patel <kmpat339@gmail.com>
2026-05-23 19:45:29 -0400

    Initial site files and .gitignore. First working version

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// const button = document.querySelector('.move-btn');
+// const box = document.querySelector('.box');
+
+// button.addEventListener('click', () => {
+//   box.classList.toggle('moved');
+// });
